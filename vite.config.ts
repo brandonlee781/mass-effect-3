6cf9e0b879a5ccd145890320526fb67ddf374481
Brandon Lee <brandonlee781@gmail.com>
2021-06-17 16:42:50 -0500

    correct base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mass-effect-3-scanning/',
+  base: '/mass-effect-3/',
   plugins: [vue()],
 })
